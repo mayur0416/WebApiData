05d447ab9ab7698fc206c099d2c110c4fc8c080b
Mayur Bhagoji Ajiwale <mayur-bhagoji.ajiwale@capgemini.com>
2022-09-09 13:07:18 +0530

    Add project files.

---
+++ WebApiData/Controllers/WeatherForecastController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiData.Models;
+using WebApiData.RequestModel;
+
+namespace WebApiData.Controllers
+{
+    [ApiController]
+    [Route("api/weather")]
+    [Produces("application/xml")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet]
+        [Route("~/weatherdata")]
+        public  IActionResult GetWeatherData(string id1,[FromQuery] WeatherModel request)
+        {          
+            
+            return this.Ok( getData());
+        }
+
+        private  Array getData()
+        {
+            var rng = new Random();
+            Array a = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            return a;
+        }
+
+    }
+}
---
+++ WebApiData/CoreModel/Request.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace WebApiData.CoreModel
+{
+    public abstract class Request<TEntity>
+         where TEntity : class
+    {
+        /// <summary>
+        /// Filtering model class.
+        /// </summary>
+        public TEntity Model { get; set; }
+    }
+}
